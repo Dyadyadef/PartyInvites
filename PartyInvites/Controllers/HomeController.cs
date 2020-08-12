e9c7e00b92c00d176ff837075bc40bd8617239d2
Андрей <zybarevandrey@gmail.com>
2020-08-12 13:07:34 +0300

    Добавьте файлы проекта.

---
+++ PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index() {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
+            return View("MyView");
+        }
+
+        public ViewResult RsvpForm() {
+            return View();
+        }
+    }
+}
